dc8d35abb56e6c51fb0f569aa049b02d011ff603
dyzn3y <kdmarshall728@gmail.com>
2023-03-02 07:37:02 -0500

    started recipe-model

--- api/recipes/recipes-model.js
+++ api/recipes/recipes-model.js
@@ -1,7 +1,8 @@
-// const db = require('../../data/db-config.js');
+const db = require('../../data/db-config');
 
-function getRecipeById(recipe_id) {
-    return Promise.resolve(`awesome recipe with id ${recipe_id}`)
+async function getRecipeById(recipe_id) {
+    const recipeRows = await db('recipes as r').where('recipe_id', recipe_id)
+    return recipeRows
 }
 
 module.exports = {
